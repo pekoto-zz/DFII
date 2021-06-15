2491493c9d9e8faa5812d750eff86bc0fafa5ed1
Graham <graham20@gmail.com>
2021-06-15 21:36:04 +0900

    Add binary search iterative

---
+++ DSA/algorithms/binary_search_iterative.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+def binary_search(arr: List[int], key: int) -> int:
+    """Returns the index of a given key in a sorted array, or -1 if key not found.
+
+    1. Get left and right.
+    2. Calculate the mid.
+    3. Depending on whether the key is bigger or smaller than mid,
+       update left of right.
+
+    Space: O(1)
+    Time: log(n)
+
+    :param arr: The sorted array to search
+    :param key: The key to search for
+    :return: The index of the key if found, -1 otherwise
+    """
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        mid = left + (right-left)//2
+
+        if key > arr[mid]:
+            left = mid+1
+        elif key < arr[mid]:
+            right = mid-1
+        else:
+            return mid
+
+    # Key not found
+    return -1
---
+++ DSA/algorithms/binary_search_iterative_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from binary_search_iterative import binary_search
+
+
+class BinarySearchIterativeTest(unittest.TestCase):
+
+    def test_binary_search_found(self):
+        index = binary_search([2, 5, 9, 12, 42, 512], 9)
+
+        self.assertEqual(2, index)
+
+    def test_binary_search_not_found(self):
+        index = binary_search([2, 5, 9, 12, 42, 512], 13)
+
+        self.assertEqual(-1, index)
+
+    def test_binary_search_found_at_leftmost_pos(self):
+        index = binary_search([2, 5, 9, 12, 42, 512], 2)
+
+        self.assertEqual(0, index)
+
+    def test_binary_search_found_at_rightmost_pos(self):
+        index = binary_search([2, 5, 9, 12, 42, 512], 512)
+
+        self.assertEqual(5, index)
+
+    def test_binary_search_found_in_odd_array(self):
+        index = binary_search([2, 5, 9, 42, 512], 5)
+
+        self.assertEqual(1, index)
+
+
+if __name__ == '__main__':
+    unittest.main()
